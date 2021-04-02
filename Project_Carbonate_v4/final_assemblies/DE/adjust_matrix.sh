9c6440f3be6d0d09dd694bed52ee8eb963d5ce1d
ssander5 <ss93@iu.edu>
2021-04-01 21:05:17 -0400

    Update adjust_matrix.sh
    fixed bugs

--- Project_Carbonate_v4/final_assemblies/DE/adjust_matrix.sh
+++ Project_Carbonate_v4/final_assemblies/DE/adjust_matrix.sh
@@ -1,17 +1,10 @@
 #join all files by name of transcript, but keep only fifth column (tpm)
 
-#TO BE TESTED
-f=`ls *kallisto/abundance.tsv | head -n 1 | sed 's|/abundance.tsv||g'`
-awk '{print $1}' $f > list
-for f in *_kallisto*/abundance.tsv; do join -1 1 -2 1 tmp <(awk '{print $1,$5}' $f) | sed "s/tpm/${f%_kallisto/abundance.tsv}/g" > tmp2; mv tmp2 tmp; done
+#copy the first column of one of your sample outputs into tmp to create a list of transcripts:
+awk '{print $1}' Sample12_treat_kallisto/abundance.tsv | sed 1d | sort > tmp
 
-#old
-#join -1 1 -2 1 <(awk '{print $1,$5}' Sample12_treat/abundance.tsv) <(awk '{print $1,$5}' Sample5_treat/abundance.tsv ) > tmp
-#join -1 1 -2 1 tmp <(awk '{print $1,$5}' Sample14_control/abundance.tsv ) > tmp2
-#join -1 1 -2 1 tmp2 <(awk '{print $1,$5}' Sample2_control/abundance.tsv ) > tmp
-
-#fix header
-sed -i 1d tmp
+#loop through the samples and merge them into the list
+for f in *_kallisto/abundance.tsv; do join -1 1 -2 1 tmp <(awk '{print $1,$5}' $f | sed 1d | sort) > tmp2; mv tmp2 tmp; done
 
 ###NEEDS TO BE CHANGED!!!###
 #change this based on your sample names to be used in samples_described
@@ -20,8 +13,6 @@ cat tmp >> matrix
 
 #cleanup
 rm tmp
-rm tmp2
 
 #make tabs
 sed -i 's/\ /\t/g' matrix
-
