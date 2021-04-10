95b386d6f825eb6f40e64e22fe5cd42ba3584cc1
Layla Freeborn <layfreeb@iu.edu>
2021-04-09 21:29:28 -0600

    Update RunTrinity.normalize.sh
    converted job header to SLURM; changed job name to RunTrinityNorm

--- Project_Carbonate_v4/input_files/RunTrinity.normalize.sh
+++ Project_Carbonate_v4/input_files/RunTrinity.normalize.sh
@@ -1,8 +1,15 @@
-#PBS -k oe
-#PBS -m abe ## Notify when abort, begin, end
-#PBS -M 
-#PBS -N RunTrinity
-#PBS -l nodes=1:ppn=16,vmem=150gb,walltime=12:00:00
+#!/bin/bash 
+
+#SBATCH -J RunTrinityNorm                                 
+#SBATCH -p general 
+#SBATCH -o filename_%j.txt
+#SBATCH -e filename_%j.err 
+#SBATCH --mail-type=FAIL,BEGIN,END 
+#SBATCH --mail-user=
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=16
+#SBATCH --mem=150gb
+#SBATCH --time=12:00:00 
 
 module load trinityrnaseq/2.6.6
 
