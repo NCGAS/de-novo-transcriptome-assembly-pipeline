d1a89c2bfb72e16f063d3957d087016c6b6d0be0
ssander5 <ss93@iu.edu>
2021-04-14 16:37:37 -0400

    Update RunTransAb1b.sh

--- Project_Carbonate_v4/TransAbyss/RunTransAb1b.sh
+++ Project_Carbonate_v4/TransAbyss/RunTransAb1b.sh
@@ -14,7 +14,8 @@
 #set wd
 cd PWDHERE/TransAbyss
 
-module load python
+module load python/3.6.8
+module load igraph/python3.6/0.9.0
 module load transabyss/2.0.1
 module load igraph/python/0.7.1
 module load blat
