952ab61b78d69b4e0b7dfa56a55bcc2bcbf94e57
ssander5 <ss93@iu.edu>
2021-04-14 16:22:44 -0400

    Update RunQC.sh

--- Project_Carbonate_v4/final_assemblies/QC/RunQC.sh
+++ Project_Carbonate_v4/final_assemblies/QC/RunQC.sh
@@ -2,8 +2,8 @@
 
 #SBATCH -J RunQC                                 
 #SBATCH -p general 
-#SBATCH -o filename_%j.txt
-#SBATCH -e filename_%j.err 
+#SBATCH -o QC_%j.log
+#SBATCH -e QC_%j.err 
 #SBATCH --mail-type=FAIL,BEGIN,END 
 #SBATCH --mail-user=
 #SBATCH --nodes=1 
