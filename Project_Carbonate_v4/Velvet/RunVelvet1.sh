42e221ea85a5e63ca9383c7760698d74b2280261
ssander5 <ss93@iu.edu>
2021-04-14 16:19:04 -0400

    Update RunVelvet1.sh

--- Project_Carbonate_v4/Velvet/RunVelvet1.sh
+++ Project_Carbonate_v4/Velvet/RunVelvet1.sh
@@ -2,8 +2,8 @@
 
 #SBATCH -J RunVelvet1                                   
 #SBATCH -p general 
-#SBATCH -o filename_%j.txt
-#SBATCH -e filename_%j.err 
+#SBATCH -o Velvet1_%j.log
+#SBATCH -e Velvet1_%j.err 
 #SBATCH --mail-type=FAIL,BEGIN,END 
 #SBATCH --mail-user=
 #SBATCH --nodes=1 
