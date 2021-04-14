a1d8bf2cf3c88f32beeeb434ac8bed0e0f73e72a
ssander5 <ss93@iu.edu>
2021-04-14 16:16:39 -0400

    Update RunTransAb1.sh

--- Project_Carbonate_v4/TransAbyss/RunTransAb1.sh
+++ Project_Carbonate_v4/TransAbyss/RunTransAb1.sh
@@ -2,8 +2,8 @@
 
 #SBATCH -J TransAb1                                    
 #SBATCH -p general 
-#SBATCH -o filename_%j.txt
-#SBATCH -e filename_%j.err 
+#SBATCH -o TransAb1_%j.txt
+#SBATCH -e TransAb1_%j.err 
 #SBATCH --mail-type=FAIL,BEGIN,END 
 #SBATCH --mail-user=
 #SBATCH --nodes=1 
