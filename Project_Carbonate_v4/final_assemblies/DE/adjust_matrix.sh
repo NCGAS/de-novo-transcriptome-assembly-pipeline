e7c5ca70be6b2ca8b2f1fd363d9ac796e7b849ec
ssander5 <ss93@iu.edu>
2021-04-15 15:32:08 -0400

    Update adjust_matrix.sh

--- Project_Carbonate_v4/final_assemblies/DE/adjust_matrix.sh
+++ Project_Carbonate_v4/final_assemblies/DE/adjust_matrix.sh
@@ -4,7 +4,7 @@
 awk '{print $1}' Sample12_treat_kallisto/abundance.tsv | sed 1d | sort > tmp
 
 #loop through the samples and merge them into the list
-for f in *_kallisto/abundance.tsv; do join -1 1 -2 1 tmp <(awk '{print $1,$5}' $f | sed 1d | sort) > tmp2; mv tmp2 tmp; done
+for f in *_kallisto/abundance.tsv; do join -1 1 -2 1 tmp <(awk '{print $1,$5}' $f | sed 1d | sort -k1,1) > tmp2; mv tmp2 tmp; done
 
 ###NEEDS TO BE CHANGED!!!###
 #change this based on your sample names to be used in samples_described
