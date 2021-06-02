ad6b7f78bcc68a3bf7fd0c730538c541030aaad4
ssander5 <ss93@iu.edu>
2021-06-01 20:45:15 -0400

    Update run_DE_analysis.pl

--- Project_v5/software/Trinity/run_DE_analysis.pl
+++ Project_v5/software/Trinity/run_DE_analysis.pl
@@ -7,7 +7,7 @@ fi
 VERSION=2.11.0
 PACKAGE=Trinity
 TOOL=/usr/local/bin/trinityrnaseq/Analysis/DifferentialExpression/run_DE_analysis.pl
-DIRECTORY=/N/slate/ss93/Full_set/transcriptome-workshop-SP2021/Project_v5/software/Trinity
+DIRECTORY=PWDHERE/software/Trinity
 STORAGE=/N/slate,/N/project,/N/u
 
 OPTIONS=$OPTIONS" -B $STORAGE"
