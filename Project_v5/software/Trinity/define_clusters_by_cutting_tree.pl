05cbdeb5ca5a5bd878d865cbcefdef4131b1c3a2
ssander5 <ss93@iu.edu>
2021-06-01 20:31:49 -0400

    Update define_clusters_by_cutting_tree.pl

--- Project_v5/software/Trinity/define_clusters_by_cutting_tree.pl
+++ Project_v5/software/Trinity/define_clusters_by_cutting_tree.pl
@@ -7,7 +7,7 @@ fi
 VERSION=2.11.0
 PACKAGE=Trinity
 TOOL=/usr/local/bin/trinityrnaseq/Analysis/DifferentialExpression/define_clusters_by_cutting_tree.pl
-DIRECTORY=/N/slate/ss93/Full_set/transcriptome-workshop-SP2021/Project_v5/software/Trinity
+DIRECTORY=PWDHERE/software/Trinity
 STORAGE=/N/slate,/N/project,/N/u
 
 OPTIONS=$OPTIONS" -B $STORAGE"
