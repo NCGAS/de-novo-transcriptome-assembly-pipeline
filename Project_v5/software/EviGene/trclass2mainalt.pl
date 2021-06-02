62f4adf17a474cf5f099ba89527b8b9bb4225ad5
ssander5 <ss93@iu.edu>
2021-06-01 20:35:25 -0400

    Update trclass2mainalt.pl

--- Project_v5/software/EviGene/trclass2mainalt.pl
+++ Project_v5/software/EviGene/trclass2mainalt.pl
@@ -7,7 +7,7 @@ fi
 VERSION=14dec30
 PACKAGE=evigene
 TOOL=/usr/local/evigene14dec30/scripts/prot/trclass2mainalt.pl
-DIRECTORY=/N/slate/ss93/Full_set/transcriptome-workshop-SP2021/Project_v5/software/EviGene
+DIRECTORY=PWDHERE/software/EviGene
 STORAGE=/N/slate,/N/project,/N/u
 
 OPTIONS=$OPTIONS" -B $STORAGE"
