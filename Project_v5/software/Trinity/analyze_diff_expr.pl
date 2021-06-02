735006c121c226ce3197d9a244ca8ae544eceb58
ssander5 <ss93@iu.edu>
2021-06-01 20:44:36 -0400

    Update analyze_diff_expr.pl

--- Project_v5/software/Trinity/analyze_diff_expr.pl
+++ Project_v5/software/Trinity/analyze_diff_expr.pl
@@ -7,7 +7,7 @@ fi
 VERSION=2.11.0
 PACKAGE=Trinity
 TOOL=/usr/local/bin/trinityrnaseq/Analysis/DifferentialExpression/analyze_diff_expr.pl
-DIRECTORY=/N/slate/ss93/Full_set/transcriptome-workshop-SP2021/Project_v5/software/Trinity
+DIRECTORY=PWDHERE/software/Trinity
 STORAGE=/N/slate,/N/project,/N/u
 
 OPTIONS=$OPTIONS" -B $STORAGE"
