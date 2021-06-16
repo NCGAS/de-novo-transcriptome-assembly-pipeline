3d3726ebd62b766131adebbea46a1d118b25de91
ssander5 <ss93@iu.edu>
2021-06-16 15:14:52 -0400

    Update pull.sh

--- Project_v5/software/BUSCO/pull.sh
+++ Project_v5/software/BUSCO/pull.sh
@@ -6,5 +6,5 @@ if [ ! $(command -v singularity) ]; then
 	module load singularity
 fi
 
-singularity pull https://depot.galaxyproject.org/singularity/busco:5.1.3--pyhdf>
+singularity pull https://depot.galaxyproject.org/singularity/busco:5.1.3--pyhdfd78af_0
 mv -v busco:* singularity-busco-5.1.3.sif
